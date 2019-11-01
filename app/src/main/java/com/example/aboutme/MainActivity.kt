ed690dbd849068d823abc17407ec9140b571e0d0
Administrator <sardar@mypaperlessoffice.com>
2019-11-02 02:23:51 +0500

    Add nickname and set the nickname on button click.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_done.setOnClickListener {
+            Log.d(this@MainActivity.javaClass.simpleName, "onCreate: Button Clicked...");
+            addNickname(it)
+        }
+
+    }
+
+    private fun addNickname(view: View){
+        tv_nickname.text = edt_nickname.text
+        tv_nickname.visibility = View.VISIBLE
+        edt_nickname.visibility = View.GONE
+        view.visibility = View.GONE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
