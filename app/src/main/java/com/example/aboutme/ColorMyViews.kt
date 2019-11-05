bfed7129539658da1fbf069ddda093d34833ba13
Administrator <sardar@mypaperlessoffice.com>
2019-11-06 01:28:02 +0500

    Add colors on boxes.

--- app/src/main/java/com/example/aboutme/ColorMyViews.kt
+++ app/src/main/java/com/example/aboutme/ColorMyViews.kt
@@ -1,12 +1,40 @@
 package com.example.aboutme
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_color_my_views.*
 
 class ColorMyViews : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_my_views)
+        val clickAbleViews :List<View> = listOf(boxOne_text,boxTwo_text,boxThree_text,boxFour_text)
+        for(item in clickAbleViews){
+            item.setOnClickListener {
+                makeColored(it)
+            }
+        }
+
+    }
+
+    private fun makeColored(it: View?) {
+        Log.d(this@ColorMyViews.javaClass.simpleName, "makeColored: ")
+        when (it?.id) {
+
+            // Boxes using Color class colors for background
+            R.id.boxOne_text -> it?.setBackgroundColor(Color.DKGRAY)
+            R.id.boxTwo_text -> it?.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.boxThree_text -> it?.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.boxFour_text -> it?.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.boxFive_text -> it?.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> it?.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
