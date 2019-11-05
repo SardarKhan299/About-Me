2ef134241b9d6e82ab67808514ad0d09b9dd3eb1
Administrator <sardar@mypaperlessoffice.com>
2019-11-06 00:57:12 +0500

    Add Color me box activity and create one box.

---
+++ app/src/main/java/com/example/aboutme/ColorMyViews.kt
@@ -0,0 +1,12 @@
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorMyViews : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_my_views)
+    }
+}
